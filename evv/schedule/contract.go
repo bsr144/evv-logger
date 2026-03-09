dcb247649fc008f0a600640ed0185b73221a93f1
bsr144 <abrahampurnomo144@gmail.com>
2026-03-09 14:06:45 +0700

    refactor(usecase): rename repository.go to contract.go for schedule and task

--- evv/schedule/contract.go
+++ evv/schedule/contract.go
@@ -6,11 +6,19 @@ import (
 	"github.com/bsr144/evv-logger/entity"
 )
 
+type ScheduleStatsResult struct {
+	Total     int
+	Missed    int
+	Upcoming  int
+	Completed int
+}
+
 type ScheduleRepository interface {
 	CreateSchedule(ctx context.Context, schedule *entity.Schedule) (*entity.Schedule, error)
 	GetScheduleByID(ctx context.Context, id int) (*entity.Schedule, error)
 	GetAllSchedules(ctx context.Context) ([]*entity.Schedule, error)
 	UpdateSchedule(ctx context.Context, schedule *entity.Schedule) (*entity.Schedule, error)
+	GetScheduleStats(ctx context.Context) (*ScheduleStatsResult, error)
 }
 
 type TaskRepository interface {
